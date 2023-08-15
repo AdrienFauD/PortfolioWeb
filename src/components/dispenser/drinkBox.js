635b9cdd401a5b0a16aed9b3642433f9131f93a1
Adrien Fauquet <adrien681@hotmail.fr>
2023-08-15 16:55:10 +0200

    update dispenser

--- src/components/dispenser/drinkBox.js
+++ src/components/dispenser/drinkBox.js
@@ -1,10 +1,30 @@
+import { useState } from "react"
+
 export default function DrinkBox(props) {
     const { product, takeItem } = props
+    const [toggle, setToggle] = useState(true)
+    const [anim, setAnim] = useState(true)
+
+    const flipFlapper = () => {
+        setTimeout(() => { toggle === true ? setToggle(false) : setToggle(true) }, 500)
+    }
 
     return (
         <>
-            <div className="drink-box" onClick={() => takeItem()} style={{ fontSize: '3em', paddingTop: '0.5em', paddingBottom: '0.5em' }}>
-                {product}
+
+            <div className="drink-box"
+                onClick={() => flipFlapper()}
+                style={{ position: "relative", fontSize: '3em' }}>
+
+                <div className="flapper">
+
+                </div>
+
+                <div className="box"
+                    onClick={() => takeItem()}>
+                    {product}
+                </div>
+
             </div>
         </>
     )
